f154a369e459d566936970eea919be85a271a41d
grafi <grafi@grafi.jp>
2013-10-15 05:17:23 +0900

    bug fix, debug function

--- sim/csim.c
+++ sim/csim.c
@@ -1,4 +1,7 @@
-#include <assert.h>
+// TODO rearange parameters
+// compile : gcc -O3 -Wall -std=c99 -lm csim.c -o csim
+
+#include <arpa/inet.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -6,6 +9,12 @@
 
 #define INST_ADDR 0x10000 // 64Kword
 #define DATA_ADDR 0x800000 // 8Mword
+
+#define NDEBUG
+#define FLG_STEP false
+#define FLG_IO_PRINTF true
+#define FLG_COUNT_INST true
+#include <assert.h>
 
 typedef uint32_t inst_t;
 
@@ -16,19 +25,21 @@ static uint32_t PC;
 static inst_t INST_MEM[INST_ADDR];
 static uint32_t DATA_MEM[DATA_ADDR];
 
+static unsigned int inst_count;
+
 static inline inst_t bits(inst_t inst, unsigned int i, unsigned int j) {
-	return inst & ((1 << (i + 1)) - (1 << j)) >> j;
+	return (inst & ((1 << (i + 1)) - (1 << j))) >> j;
 }
 
-static inline void set_gpr(uint32_t *tag, uint32_t v) {
-	if (tag != &GPR[0]) *tag = v;
+static inline void set_gpr(inst_t tag, uint32_t v) {
+	if (tag) GPR[tag] = v;
 }
 
-static inline void set_fpr(float *tag, float v) {
-	if (tag != &FPR[0]) *tag = v;
+static inline void set_fpr(inst_t tag, float v) {
+	if (tag) FPR[tag] = v;
 }
 
-static inline void set_fpr_sgn(float *tag, inst_t sgn, float v) {
+static inline void set_fpr_sgn(inst_t tag, inst_t sgn, float v) {
 	// TODO sgn
 	float w;
 	switch (sgn) {
@@ -45,7 +56,7 @@ static inline void set_fpr_sgn(float *tag, inst_t sgn, float v) {
 		w = copysign(v, -1);
 		break;
 	}
-	if (tag != &FPR[0]) *tag = w;
+	if (tag) FPR[tag] = w;
 }
 
 static union {
@@ -64,44 +75,45 @@ static inline float int_as_float(int32_t i) {
 }
 
 static void issue();
+static void step();
 
-static void alu(inst_t code, uint32_t *ptrD, uint32_t a, uint32_t ub, int32_t sb) {
+static void alu(inst_t code, inst_t tagD, uint32_t a, uint32_t ub, int32_t sb) {
 	switch (code) {
 	case 0b0000:
-		set_gpr(ptrD, a + ub);
+		set_gpr(tagD, a + ub);
 		return issue();
 	case 0b0001:
-		set_gpr(ptrD, a - ub);
+		set_gpr(tagD, a - ub);
 		return issue();
 	case 0b0010:
-		set_gpr(ptrD, (int32_t) a == sb);
+		set_gpr(tagD, ((int32_t) a) == sb);
 		return issue();
 	case 0b0011:
-		set_gpr(ptrD, (int32_t) a < sb);
+		set_gpr(tagD, ((int32_t) a) < sb);
 		return issue();
 	case 0b0100:
-		set_gpr(ptrD, a & sb);
+		set_gpr(tagD, a & sb);
 		return issue();
 	case 0b0101:
-		set_gpr(ptrD, a | sb);
+		set_gpr(tagD, a | sb);
 		return issue();
 	case 0b0110:
-		set_gpr(ptrD, a ^ sb);
+		set_gpr(tagD, a ^ sb);
 		return issue();
 	case 0b0111:
-		set_gpr(ptrD, a << bits(ub, 4, 0));
+		set_gpr(tagD, a << bits(ub, 4, 0));
 		return issue();
 	case 0b1000:
-		set_gpr(ptrD, (uint32_t) a >> bits(ub, 4, 0));
+		set_gpr(tagD, ((uint32_t) a) >> bits(ub, 4, 0));
 		return issue();
 	case 0b1001:
-		set_gpr(ptrD, a >> bits(ub, 4, 0)); // relying on undefined behavior
+		set_gpr(tagD, a >> bits(ub, 4, 0)); // relying on undefined behavior
 		return issue();
 	case 0b1010:
-		set_gpr(ptrD, (a & ((1 << 16) - 1)) | (ub << 16));
+		set_gpr(tagD, (a & ((1 << 16) - 1)) | (ub << 16));
 		return issue();
 	case 0b1011:
-		set_gpr(ptrD, a * ub);
+		set_gpr(tagD, a * ub);
 		return issue();
 	default:
 		assert(false);
@@ -109,21 +121,21 @@ static void alu(inst_t code, uint32_t *ptrD, uint32_t a, uint32_t ub, int32_t sb
 	}
 }
 
-static void aluf(inst_t code, uint32_t *ptrD, float a, float b) {
+static void aluf(inst_t code, inst_t tagD, float a, float b) {
 	switch (code) {
 	case 0b1100:
 		assert(b == 0.0);
-		set_gpr(ptrD, (uint32_t) float_as_int(a));
+		set_gpr(tagD, (uint32_t) float_as_int(a));
 		return issue();
 	case 0b1101:
 		assert(b == 0.0);
-		set_gpr(ptrD, (uint32_t) (int32_t) a);
+		set_gpr(tagD, (uint32_t) (int32_t) a);
 		return issue();
 	case 0b1110:
-		set_gpr(ptrD, a == b);
+		set_gpr(tagD, a == b);
 		return issue();
 	case 0b1111:
-		set_gpr(ptrD, a < b);
+		set_gpr(tagD, a < b);
 		return issue();
 	default:
 		assert(false);
@@ -131,28 +143,28 @@ static void aluf(inst_t code, uint32_t *ptrD, float a, float b) {
 	}
 }
 
-static void fpu(inst_t code, inst_t sgn, float *ptrD, float a, float b) {
+static void fpu(inst_t code, inst_t sgn, inst_t tagD, float a, float b) {
 	switch (code) {
 	case 0b000:
-		set_fpr_sgn(ptrD, sgn, a + b);
+		set_fpr_sgn(tagD, sgn, a + b);
 		return issue();
 	case 0b001:
-		set_fpr_sgn(ptrD, sgn, a - b);
+		set_fpr_sgn(tagD, sgn, a - b);
 		return issue();
 	case 0b010:
-		set_fpr_sgn(ptrD, sgn, a * b);
+		set_fpr_sgn(tagD, sgn, a * b);
 		return issue();
 	case 0b011:
 		assert(b == 0.0);
-		set_fpr_sgn(ptrD, sgn, 1 / a);
+		set_fpr_sgn(tagD, sgn, 1 / a);
 		return issue();
 	case 0b100:
 		assert(b == 0.0);
-		set_fpr_sgn(ptrD, sgn, sqrtf(a));
+		set_fpr_sgn(tagD, sgn, sqrtf(a));
 		return issue();
 	case 0b101:
 		assert(b == 0.0);
-		set_fpr_sgn(ptrD, sgn, a);
+		set_fpr_sgn(tagD, sgn, a);
 		return issue();
 	default:
 		assert(false);
@@ -160,15 +172,15 @@ static void fpu(inst_t code, inst_t sgn, float *ptrD, float a, float b) {
 	}
 }
 
-static void fpur(inst_t code, inst_t sgn, float *ptrD, uint32_t a, uint32_t b) {
+static void fpur(inst_t code, inst_t sgn, inst_t tagD, uint32_t a, uint32_t b) {
 	switch (code) {
 	case 0b110:
 		assert(b == 0);
-		set_fpr_sgn(ptrD, sgn, int_as_float((int32_t) a));
+		set_fpr_sgn(tagD, sgn, int_as_float((int32_t) a));
 		return issue();
 	case 0b111:
 		assert(b == 0);
-		set_fpr_sgn(ptrD, sgn, ((float) (int32_t) a));
+		set_fpr_sgn(tagD, sgn, ((float) (int32_t) a));
 		return issue();
 	default:
 		assert(false);
@@ -216,55 +228,66 @@ static void brf(inst_t code, uint32_t target, float a, float b) {
 	}
 }
 
-static void rrsp(inst_t func, uint32_t *ptrA, uint32_t b) {
+static void rrsp(inst_t func, inst_t tagA, uint32_t b) {
 	switch (func) {
-		int i;
 		case 0:
-			for (i = 0; i < 4; i++) {
-				putchar((char) b >> 24);
-				b = b >> 8;
-			}
-			return issue();
-		case 1:
-			for (i = 0; i < 4; i++) {
+			assert(b == 0);
+			for (int i = 0; i < 4; i++) {
 				b = b << 8;
 				b &= (unsigned char) getchar();
 			}
-			*ptrA = b;
+			set_gpr(tagA, b);
+			return issue();
+		case 1:
+			assert(tagA == 0);
+			if (FLG_IO_PRINTF) {
+				printf("%u\n", b);
+			} else {
+				for (int i = 0; i < 4; i++) {
+					putchar((unsigned char) (b >> 24));
+					b = b << 8;
+				}
+			}
 			return issue();
 		default:
-			assert(false);
-			return issue();
+			// halt signal
+			return;
 	}
 }
 
-static void rfsp(inst_t func, uint32_t *ptrA, float b) {
+static void rfsp(inst_t func, inst_t tagA, float b) {
 	assert(false);
 	return issue();
 }
 
-static void frsp(inst_t func, float *ptrA, uint32_t b) {
+static void frsp(inst_t func, inst_t tagA, uint32_t b) {
 	assert(false);
 	return issue();
 }
 
-static void ffsp(inst_t func, float *ptrA, float b) {
+static void ffsp(inst_t func, inst_t tagA, float b) {
 	switch (func) {
 		uint32_t tmp;
 		int i;
 		case 0:
-			tmp = float_as_int(b);
-			for (i = 0; i < 4; i++) {
-				putchar((char) tmp >> 24);
-				tmp = tmp >> 8;
-			}
-			return issue();
-		case 1:
+			assert(b == 0.0);
 			for (i = 0; i < 4; i++) {
 				tmp = tmp << 8;
 				tmp &= (unsigned char) getchar();
 			}
-			*ptrA = int_as_float(tmp);
+			set_fpr(tagA, int_as_float((int32_t) tmp));
+			return issue();
+		case 1:
+			assert(tagA == 0);
+			tmp = float_as_int(b);
+			if (FLG_IO_PRINTF) {
+				printf("%f\n", b);
+			} else {
+				for (i = 0; i < 4; i++) {
+					putchar((char) tmp >> 24);
+					tmp = tmp >> 8;
+				}
+			}
 			return issue();
 		default:
 			assert(false);
@@ -273,6 +296,8 @@ static void ffsp(inst_t func, float *ptrA, float b) {
 }
 
 static void issue() {
+	if (FLG_STEP) step();
+	if (FLG_COUNT_INST) inst_count++;
 	inst_t inst = INST_MEM[PC++];
 
 	inst_t tagA = bits(inst, 25, 21);
@@ -282,51 +307,51 @@ static void issue() {
 
 	switch (bits(inst, 31, 30)) {
 	case 0b00:
-		return alu(bits(inst, 29, 26), &GPR[tagA], GPR[tagB], imm, (int32_t) imm);
+		return alu(bits(inst, 29, 26), tagA, GPR[tagB], imm, (int32_t) (int16_t) imm);
 	case 0b01:
 		switch (bits(inst, 29, 26)) {
 			case 0b0000:
-				return alu(bits(inst, 3, 0), &GPR[tagA], GPR[tagB], GPR[tagC], (int32_t) GPR[tagC]);
+				return alu(bits(inst, 3, 0), tagA, GPR[tagB], GPR[tagC], (int32_t) GPR[tagC]);
 			case 0b0001:
-				return aluf(bits(inst, 3, 0), &GPR[tagA], FPR[tagB], FPR[tagC]);
+				return aluf(bits(inst, 3, 0), tagA, FPR[tagB], FPR[tagC]);
 			case 0b0010:
 				assert(imm == 0);
-				GPR[tagB] = PC << 2;
-				PC = GPR[tagA] >> 2 & (INST_ADDR - 1);
+				set_gpr(tagA, PC << 2);
+				PC = GPR[tagB] >> 2 & (INST_ADDR - 1);
 				return issue();
 			case 0b0011:
-				assert(tagA == 0);
-				GPR[tagB] = PC << 2;
+				assert(tagB == 0);
+				set_gpr(tagA, PC << 2);
 				PC = imm;
 				return issue();
 			case 0b1000:
-				return fpur(bits(inst, 3, 0), bits(inst, 5, 4), &FPR[tagA], GPR[tagB], GPR[tagC]);
+				return fpur(bits(inst, 3, 0), bits(inst, 5, 4), tagA, GPR[tagB], GPR[tagC]);
 			case 0b1001:
-				return fpu(bits(inst, 3, 0), bits(inst, 5, 4), &FPR[tagA], FPR[tagB], FPR[tagC]);
+				return fpu(bits(inst, 3, 0), bits(inst, 5, 4), tagA, FPR[tagB], FPR[tagC]);
 			case 0b0100:
-				return rrsp(imm, &GPR[tagA], GPR[tagB]);
+				return rrsp(imm, tagA, GPR[tagB]);
 			case 0b0101:
-				return rfsp(imm, &GPR[tagA], FPR[tagB]);
+				return rfsp(imm, tagA, FPR[tagB]);
 			case 0b1100:
-				return frsp(imm, &FPR[tagA], GPR[tagB]);
+				return frsp(imm, tagA, GPR[tagB]);
 			case 0b1101:
-				return ffsp(imm, &FPR[tagA], GPR[tagB]);
+				return ffsp(imm, tagA, GPR[tagB]);
 			default:
 				assert(false);
 				return issue();
 		}
 	case 0b10:
 		;
-		uint32_t addr = (GPR[tagB] >> 2 & (DATA_ADDR - 1)) + imm;
+		uint32_t addr = ((GPR[tagB] >> 2) + ((int32_t) (int16_t) imm)) & (DATA_ADDR - 1);
 		switch (bits(inst, 29, 26)) {
 			case 0b0000:
-				set_gpr(&GPR[tagA], DATA_MEM[addr]);
+				set_gpr(tagA, DATA_MEM[addr]);
 				return issue();
 			case 0b0001:
 				DATA_MEM[addr] = GPR[tagA];
 				return issue();
 			case 0b1000:
-				set_gpr(&GPR[tagA], int_as_float((int32_t) DATA_MEM[addr]));
+				set_fpr(tagA, int_as_float((int32_t) DATA_MEM[addr]));
 				return issue();
 			case 0b1001:
 				DATA_MEM[addr] = (uint32_t) float_as_int(FPR[tagA]);
@@ -345,12 +370,30 @@ static void issue() {
 	}
 }
 
+static void step() {
+	printf("pc: %d\n", PC);
+	while (true) {
+		char buf[10];
+		gets(buf);
+		unsigned int v;
+		if (sscanf(buf, "r%u", &v) == 1 && v < 32) {
+			printf("r%d: %d\n", v, GPR[v]);
+			continue;
+		}
+		if (sscanf(buf, "mem%u", &v) == 1) {
+			printf("mem%d: %d\n", v, DATA_MEM[v]);
+			continue;
+		}
+		break;
+	}
+}
+
 int main(int argc, const char *argv[]) {
-	if (argc != 1) {
+	if (argc != 2) {
 		fputs("extactly one source file must be specified", stderr);
 		return 1;
 	}
-	FILE *fp = fopen(argv[0], "r");
+	FILE *fp = fopen(argv[1], "r");
 	if (!fp) {
 		fputs("opening file failed", stderr);
 		return 1;
@@ -358,11 +401,15 @@ int main(int argc, const char *argv[]) {
 
 	size_t cnt;
 	size_t pos = 0;
-	while ((cnt = fread(&INST_MEM[pos], 4, 2048, fp))) {
+	while ((cnt = fread(&INST_MEM[pos], 4, 2048, fp)))
 		pos += cnt;
-	}
+	for (size_t i = 0; i < pos; i++)
+		INST_MEM[i] = htonl(INST_MEM[i]);
 
 	issue();
+	if (FLG_COUNT_INST) {
+		printf("instruction count %u\n", inst_count);
+	}
 
 	return 0;
 }
