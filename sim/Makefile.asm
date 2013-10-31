40b9ec8ce9638b22b3de7543a016547e99fe3e3c
grafi <grafi@grafi.sakura.ne.jp>
2013-11-01 01:13:01 +0900

    using `asm` instead of `asen`

---
+++ sim/Makefile.asm
@@ -0,0 +1,4 @@
+SOURCES = lexer.mll parser.mly type.ml top.ml print.ml asm.ml asmMain.ml
+RESULT = myasm
+
+-include OCamlMakefile
--- sim/asmMain.ml
+++ sim/asmMain.ml
@@ -1,4 +1,4 @@
 let _ =
   let (env, exprs) = Top.get_top() in
-  let insts = Asen.compile env exprs in
+  let insts = Asm.compile env exprs in
   Array.iter Print.print_binary insts
