b9aab39c76923903a607ba4c36a2f3372c6d13f1
grafi <grafi@grafi.jp>
2013-11-19 02:34:42 +0900

    fix len_raw bits width

--- FPU/FFlr.vhd
+++ FPU/FFlr.vhd
@@ -20,19 +20,19 @@ architecture Implementation of FFlr is
 
 begin
     len_raw <= unsigned('0' & f(30 downto 23)) - "001111111";
-    len <= "00000" when len_raw(9) = '1' else
-           "11111" when len_raw(8 downto 5) /= "000" else
+    len <= "00000" when len_raw(8) = '1' else
+           "11111" when len_raw(7 downto 5) /= "000" else
            len_raw(4 downto 0);
     x_mask <= shift_right("000" & x"07FFFFF", to_integer(len));
-    incr <= (x_mask(30 downto 0) & '1') xor x_mask;
+    incr <= (x_mask(29 downto 0) & '1') xor x_mask;
 
     f_masked <= unsigned(f(30 downto 0)) and not x_mask;
     f_incr <= f_masked + incr;
     f_out <= f_masked when f(31) = '0' or unsigned(f(30 downto 0)) = f_masked else
              f_incr;
 
-    res <= f(31) & "00000000" & f_out(22 downto 0) when (f(31) = '0' or f(30 downto 23) = "00000000") and len_raw(9) = '1' else
-           f(31) & "01111111" & f_out(22 downto 0) when f(31) = '1' and len_raw(9) = '1' else
+    res <= f(31) & "00000000" & f_out(22 downto 0) when (f(31) = '0' or f(30 downto 23) = "00000000") and len_raw(8) = '1' else
+           f(31) & "01111111" & f_out(22 downto 0) when f(31) = '1' and len_raw(8) = '1' else
            f(31) & f_out;
 
     pipe : process(clk)
